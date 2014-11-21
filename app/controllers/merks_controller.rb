f06403fd36c5103ba5b85fb4b3454fa025df5e1b
Wildan Prabowo <wildan.p@kiranatama.com>
2014-11-21 08:22:28 -0800

    completed CRUD merk n products

--- app/controllers/merks_controller.rb
+++ app/controllers/merks_controller.rb
@@ -1,8 +1,12 @@
 class MerksController < ApplicationController
   before_action :authenticate_user!
+  before_filter :prepare_params, only: [:edit, :show]
 
   def index
     @merks = Merk.all
+  end
+
+  def show
   end
 
   def new
@@ -20,9 +24,38 @@ class MerksController < ApplicationController
     redirect_to merks_path, :notice => notice
   end
 
+  def edit
+  end
+
+  def update
+    merk = Merk.find(params[:id])
+
+    if merk.update_attributes(merk_params)
+      notice = "Merk berhasil di update."
+    else
+      notice = "Merk gagal di update. Hubungi administrator."
+    end
+    redirect_to merks_path, :notice => notice
+  end
+
+  def destroy
+    merk = Merk.find(params[:id])
+
+    if merk.destroy
+      notice = "Merk berhasil di hapus"
+    else
+      notice = "Merk gagal di hapus. hubungi administrator"
+    end
+    redirect_to merks_path, :notice => notice
+  end
+
   private
 
   def merk_params
     params.require(:merk).permit(:name, :background)
+  end
+
+  def prepare_params
+    @merk = Merk.find(params[:id])
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,60 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!, except: [:show]
+  before_filter :prepare_params, only: [:edit, :show]
 
   def index
+    @products = Product.all
   end
 
   def new
-    
+    @product = Product.new
+    @merks = Merk.all
+  end
+
+  def create    
+    product = Product.new(product_params)
+
+    if product.save
+      notice = "Product berhasil di buat."
+    else
+      notice = "Product gagal di buat. Hubungi administrator."
+    end
+    redirect_to products_path, :notice => notice
+  end
+
+  def edit
+    @merks = Merk.all
+  end
+
+  def update
+    product = Product.find(params[:id])
+
+    if product.update_attributes(product_params)
+      notice = "Product berhasil di update."
+    else
+      notice = "Product gagal di update. Hubungi administrator."
+    end
+    redirect_to products_path, :notice => notice
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+
+    if product.destroy
+      notice = "Product berhasil di hapus"
+    else
+      notice = "Product gagal di hapus. hubungi administrator"
+    end
+    redirect_to products_path, :notice => notice
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :merk_id, :image)
+  end
+
+  def prepare_params
+    @product = Product.find(params[:id])
   end
 end
